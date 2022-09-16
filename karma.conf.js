be8a6a786e6355ff9156ffb8c9010d1127522efd
Rasovi <halura@hotmail.com>
2022-09-17 00:10:33 +0200

    Cambio de nombre

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/acciones'),
+      dir: require('path').join(__dirname, './coverage/stockTracker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'acciones'`, () => {
+  it(`should have as title 'stockTracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('acciones');
+    expect(app.title).toEqual('StockTracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('acciones app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('stockTracker app is running!');
   });
 });
